7844fe6c07f11bda5ecd02431b767dc4e4b2229b
EricLegaria <e_legaria@outlook.com>
2020-09-11 10:47:27 -0500

    Conexion a base de datos, creacion de usuario y documento de usuario

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -12,7 +12,10 @@ const SignIn = () => {
     });
 
     const handleChange = (event) =>{
-        setLogin({...login, [event.target.name] : event.target.value});
+        setLogin({
+            ...login, [event.target.name] 
+            : event.target.value
+        });
     }
 
     const sendData = async (event) => {
