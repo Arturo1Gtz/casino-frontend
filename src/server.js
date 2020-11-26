a95e66dbd75d5896fd3455306efcd22820466d1e
Diego Flores <dfmonroy07@gmail.com>
2020-11-25 18:56:01 -0600

    updated server commands

--- src/server.js
+++ src/server.js
@@ -94,6 +94,6 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = 3001 || process.env.PORT;
+const PORT = 8081 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
