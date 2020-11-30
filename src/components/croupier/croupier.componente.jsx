5c957d56e0d880ede6a03240c7005a8ad6824c91
Diego Flores <dfmonroy07@gmail.com>
2020-11-30 11:25:52 -0600

    fixed calc vueltas

--- src/components/croupier/croupier.componente.jsx
+++ src/components/croupier/croupier.componente.jsx
@@ -85,8 +85,8 @@ class Croupier extends React.Component{
                 this.setState({vueltasIn:vint},() => console.log('calamares',this.state));
             });
 
-            this.setState_(vueltasEx = Math.random() * (25- 10) + 10);
-            this.setState(vueltasIn = Math.random() * (25- 10) + 10);
+            this.setState({vueltasEx: Math.random() * (25- 10) + 10});
+            this.setState({vueltasIn: Math.random() * (25- 10) + 10});
 
             this.setState({onGame:true, onGiro:true}
              ,() => console.log('vueltas',this.state)
